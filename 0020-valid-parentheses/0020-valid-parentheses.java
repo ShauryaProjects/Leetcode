63a8a14b2674de873c838db00def832801277825
Shaurya Srivastava <shauryasrivastav07@gmail.com>
2026-09-06 17:23:55 +0530

    Time: 3 ms (86.05%), Space: 43.1 MB (85.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+
+            else {
+                
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (c == ')' && stack.peek() != '(') {
+                    return false;
+                }
+
+                if (c == ']' && stack.peek() != '[') {
+                    return false;
+                }
+
+                if (c == '}' && stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
